c03579dcd4b962ea2b1ac2b633c09c2040b4dbac
Naveed <08es34@gmail.com>
2016-01-20 15:41:17 +0500

    initial

---
+++ src/Html.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by naveedulhassan.
+ * Date: 1/20/16
+ * Time: 3:25 PM
+ */
+
+namespace Nvd\Crud;
+
+
+class Html
+{
+    public static function test()
+    {
+        return "test";
+    }
+}
