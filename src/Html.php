36105a25a230dba9d7f7b71c1b0e3ba00d19e62a
Naveed <08es34@gmail.com>
2016-01-21 10:10:31 +0500

    search + sort complete

--- src/Html.php
+++ src/Html.php
@@ -8,16 +8,64 @@ class Html
 {
     public static function sortableTh($fieldName, $route, $label=null )
     {
-        $label = $label?:ucwords( str_replace( "_", " ", $fieldName ) );
-        $sortType = Request::input("sortType") == "asc" ? "desc" : "asc";
+        $output = "<th>";
+
+        $sortType = "asc";
+        if( Request::input("sort") == $fieldName and Request::input("sortType") == "asc" )
+            $sortType = "desc";
         $params = array_merge(Input::query(),['sort' => $fieldName, 'sortType' => $sortType]);
         $href = route($route,$params);
-
-        $output = "<th>";
         $output .= "<a href='{$href}'>";
+
+        $label = $label?:ucwords( str_replace( "_", " ", $fieldName ) );
         $output .= $label;
+
+        if( Request::input("sort") == $fieldName )
+            $output .= " <i class='fa fa-sort-alpha-".Request::input("sortType")."'></i>";
+
         $output .= "</a>";
         $output .= "</th>";
         return $output;
     }
+
+    public static function selectRequested($name, $options, $attributes = [], $useKeysAsValues = false)
+    {
+        return static::select($name, $options, $attributes, \Request::input($name), $useKeysAsValues);
+    }
+
+    public static function select( $name, $options, $attributes = [], $selectedValue = null, $useKeysAsValues = false )
+    {
+        $output = static::startTag( "select", array_merge( ['name'=>$name], $attributes ) );
+        foreach ( $options as $key => $value ){
+            if( $useKeysAsValues )
+            {
+                $selectedAttr = $key === $selectedValue ? " selected" : "";
+                $valueAttr = " value='{$key}'";
+            }
+            else
+            {
+                $selectedAttr = $value === $selectedValue ? " selected" : "";
+                $valueAttr = "";
+            }
+            $output .= "<option{$selectedAttr}{$valueAttr}>{$value}</option>";
+        }
+        $output .= static::endTag("select");
+        return $output;
+    }
+
+    public static function startTag( $tag, $attributes = [] )
+    {
+        $output = "<{$tag}";
+        foreach ( $attributes as $attr => $value ){
+            $output .= " {$attr}='{$value}'";
+        }
+        $output .= ">";
+        return $output;
+    }
+
+    public static function endTag( $tag )
+    {
+        return "</{$tag}>";
+    }
+
 }
